643caba108f10be1dad7dad674b319fc0bd8593e
IceNeoMax <study4fun2011@gmail.com>
2016-11-22 14:55:04 +0700

    fb&gg login with auth0

---
+++ 04-user-profile/app/auth.config.ts
@@ -0,0 +1,9 @@
+interface AuthConfiguration {
+    clientID: string,
+    domain: string
+}
+
+export const myConfig: AuthConfiguration = {
+    clientID: 'bigMj9EzuulYQHirm1FqxjfRNQXYXoLP',
+    domain: 'study4fun2011.auth0.com'
+};
